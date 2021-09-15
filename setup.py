8f614685c82ab77f9ac783ccdd652384f1d9e94e
Birger Moell <birger.moell@gmail.com>
2021-09-15 11:24:45 +0200

    Updated version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.0.60'
+VERSION = '0.0.61'
 DESCRIPTION = 'TMH Speech package'
 LONG_DESCRIPTION = 'A package for TMH Speach'
 
