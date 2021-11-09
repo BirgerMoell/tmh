a8d8e9421f24a84cde724d5f30b3f140aff12b19
hfkml <harm0104@hotmail.ca>
2021-11-09 15:27:11 +0100

    The phonemizer can now be run from the terminal

--- tmh/phonemes.py
+++ tmh/phonemes.py
@@ -1,11 +1,37 @@
 from dp.phonemizer import Phonemizer
 from dp.model import model, predictor
 import torch
+import argparse
 
 # this model needs a download of the model.
 ## Please download this model and put it in the following folder.
 ## https://public-asai-dl-models.s3.eu-central-1.amazonaws.com/DeepPhonemizer/en_us_cmudict_ipa_forward.pt
 ## The model assumes that the model is stored in the current folder.
+
+parser = argparse.ArgumentParser(description='Phonemize Swedish or English text')
+parser.add_argument("--model_path", metavar='M', type=str, nargs='?', default='./en_us_cmudict_ipa_forward.pt',
+                    help='The model path to your saved checkpoint')
+parser.add_argument('--language', metavar='L', type=str, default='English',
+                    help='The language you would like to phonemize')
+parser.add_argument('--stress', nargs='?', metavar='S', type=str, default="n",
+                    help='Indicate whether your model includes stress mark (y or n)')
+parser.add_argument('--text', metavar="T", type=str,
+                    help="The text you would like to phonemize formatted as a single string")
+parser.add_argument('--save_to_file', nargs='?', metavar='F', type=str, 
+                    help='If you want to save the phonemized text, write the filepath')
+args = parser.parse_args()
+
+def str_to_bool(string):
+    if string == "y":
+        return True
+    if string == "n":
+        return False
+
+model_path = args.model_path
+language = args.language
+stress = str_to_bool(args.stress)
+text = args.text
+save_path = args.save_to_file
 
 def get_swedish_phonemes(text, model_path, stress_marks=False):
     checkpoint = model_path
@@ -31,7 +57,7 @@ def get_swedish_phonemes(text, model_path, stress_marks=False):
             result = result[:-1] + ph + ' ' 
     return result
 
-def get_phonemes(text, model_checkpoint='./en_us_cmudict_ipa_forward.pt', language='English', stress_marks=False):    
+def get_phonemes(text, model_checkpoint='./en_us_cmudict_ipa_forward.pt', language='English', stress_marks=False):   
     if language == 'English':
         phonemizer = Phonemizer.from_checkpoint(model_checkpoint)
         phonemes = phonemizer(text, lang='en_us')
@@ -40,3 +66,12 @@ def get_phonemes(text, model_checkpoint='./en_us_cmudict_ipa_forward.pt', langua
         return get_swedish_phonemes(text, model_checkpoint, stress_marks=True)
     else:
         return get_swedish_phonemes(text, model_checkpoint)
+
+if __name__=='__main__':
+    if save_path:
+        phones = get_phonemes(text, model_path, language, stress)
+        with open(save_path, "a", encoding="utf-8") as outfile:
+            outfile.write(phones + '\n')
+
+    else:
+        print(get_phonemes(text, model_path, language, stress))
