dd6a5a07a4c406050d5370c002e63875c5d1fca4
Birger Moell <birger.moell@gmail.com>
2022-03-01 15:52:13 +0100

    Added pydub

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.0.80'
+VERSION = '0.0.81'
 DESCRIPTION = 'TMH Speech package'
 LONG_DESCRIPTION = 'A package for TMH Speech'
 
@@ -22,7 +22,7 @@ setup(
     long_description=long_description,
     packages=find_packages(),
     install_requires=['transformers', 'torch', 'torchaudio', 'speechbrain',
-                      'librosa', 'numpy', 'scipy', 'unidecode', 'inflect', 'librosa', 'python-dotenv', 'deep-phonemizer', 'argparse'],
+                      'librosa', 'numpy', 'scipy', 'unidecode', 'inflect', 'librosa', 'python-dotenv', 'deep-phonemizer', 'argparse', 'pydub'],
     keywords=['python', 'speech', 'voice'],
     classifiers=[
         "Development Status :: 1 - Planning",
