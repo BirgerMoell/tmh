6c09d98335656dcbd04365c1aa470508da5ebca1
Birger Moell <birger.moell@gmail.com>
2022-06-03 10:02:32 +0200

    Added package dependency

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.0.95'
+VERSION = '0.0.96'
 DESCRIPTION = 'TMH Speech package'
 LONG_DESCRIPTION = 'A package for TMH Speech'
 
@@ -22,7 +22,7 @@ setup(
     long_description=long_description,
     packages=find_packages(),
     install_requires=['transformers', 'torch', 'torchaudio', 'speechbrain',
-                      'librosa', 'numpy', 'scipy', 'unidecode', 'inflect', 'librosa', 'python-dotenv', 'deep-phonemizer', 'argparse', 'pydub'],
+                      'librosa', 'numpy', 'scipy', 'unidecode', 'inflect', 'librosa', 'python-dotenv', 'deep-phonemizer', 'argparse', 'pydub', 'pyctcdecode'],
     keywords=['python', 'speech', 'voice', 'nlp'],
     classifiers=[
         "Development Status :: 1 - Planning",
