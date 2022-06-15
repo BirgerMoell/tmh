ac5228a29390c353b73f93a96df005b0868ac070
erseux <ERSEUSAUGUST@GMAIL.COM>
2022-06-15 12:38:30 +0200

    use utils file instead

--- tmh/transcribe_with_lm.py
+++ tmh/transcribe_with_lm.py
@@ -1,5 +1,4 @@
 from lib2to3.pytree import convert
-from xdrlib import ConversionError
 import torchaudio
 import torch
 from itertools import groupby
@@ -12,30 +11,12 @@ import soundfile as sf
 import os
 import numpy as np
 from tmh.transcribe_with_vad import extract_speak_segments
+from utils import change_sample_rate, ensure_wav
 
 # from language_files import get_model
-def change_sample_rate(audio_path, new_sample_rate=16000):
-    audio_to_resample, sr = librosa.load(audio_path)
-    resampled_audio = librosa.resample(audio_to_resample, sr, new_sample_rate)
-    resampled_tensor = torch.tensor([resampled_audio])
-    return resampled_tensor
-
-def convert_to_wav(audio_path):
-    path = audio_path.split("/")
-    filename = path[-1].split(".")[0]
-    audio, sr = librosa.load(audio_path)
-    wav_path = f'{filename}.wav'
-    sf.write(wav_path, audio, sr, subtype='PCM_24')
-    return wav_path
 
 def transcribe_from_audio_path_with_lm(audio_path, model_id="viktor-enzell/wav2vec2-large-voxrex-swedish-4gram"):
-    converted = False
-    if audio_path[-4:] != ".wav":
-        try:
-            audio_path = convert_to_wav(audio_path)
-            converted = True
-        except:
-            raise ConversionError(f"Could not convert {audio_path} to wav")
+    audio_path, converted = ensure_wav(audio_path)
     
     waveform, sample_rate = torchaudio.load(audio_path)
     if sample_rate != 16000:
@@ -63,13 +44,7 @@ def transcribe_from_audio_path_with_lm(audio_path, model_id="viktor-enzell/wav2v
 
 
 def transcribe_from_audio_path_with_lm_vad(audio_path, model_id="viktor-enzell/wav2vec2-large-voxrex-swedish-4gram"):
-    converted = False
-    if audio_path[-4:] != ".wav":
-        try:
-            audio_path = convert_to_wav(audio_path)
-            converted = True
-        except:
-            raise ConversionError(f"Could not convert {audio_path} to wav")
+    audio_path, converted = ensure_wav(audio_path)
     
     waveform, sample_rate = torchaudio.load(audio_path)
     if sample_rate != 16000:
